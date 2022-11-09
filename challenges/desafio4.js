53d5b7fc258f46abe5037e8bb6034ce58dde3668
Otávio Azevedo <otavioad1997@gmail.com>
2022-11-09 18:34:41 -0300

    Correções Lint

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,8 +1,8 @@
 db.produtos.find({
     $and: [
-      {vendidos: { $gt: 50 }},
-      {vendidos: { $lt: 100 }}
-    ]
+      { vendidos: { $gt: 50 } },
+      { vendidos: { $lt: 100 } },
+    ],
   }, { _id: 0, nome: 1, vendidos: 1 })
   .sort({ vendidos: 1 })
   .pretty();
