19e92b2c11fc702a1edca6c80ff51c9bbde120d9
Otávio Azevedo <otavioad1997@gmail.com>
2022-11-10 18:18:23 -0300

    Desafio 23

---
+++ challenges/desafio22.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+    {},
+    { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
+  );
+  db.produtos.updateOne( 
+    { nome: "Big Mac" }, 
+    { $set: { "vendasPorDia.3": 60 } },
+  );
+  db.produtos.updateMany(
+    { tags: { $in: ["bovino"] } },
+    { $set: { "vendasPorDia.7": 120 } },
+  );
+  db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
